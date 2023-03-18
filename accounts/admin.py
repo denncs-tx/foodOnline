d62d7bddbabe400a0c663d1cb4148e6077d7e57d
denncs-tx <denncs@verizon.net>
2023-03-18 09:24:12 -0500

    Authentication with profiles and Django hooks implemented.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from accounts.models import User
+from accounts.models import User, UserProfile
 
 
 # Register your models here.
@@ -14,3 +14,4 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
