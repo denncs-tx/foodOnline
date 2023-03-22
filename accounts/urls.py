beb4d0886d679dcd99f9c22cae93d12f79f258fc
denncs-tx <denncs@verizon.net>
2023-03-22 06:44:11 -0500

    Vendor Registration functionality completed.

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('registerUser/', views.registerUser, name='registerUser')
+    path('registerUser/', views.registerUser, name='registerUser'),
+    path('registerVendor/', views.registerVendor, name='registerVendor'),
 ]
