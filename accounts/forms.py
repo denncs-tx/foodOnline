6423d6f30b6d5c9b262d33c1d3bd3fe5b0a0243d
denncs-tx <denncs@verizon.net>
2023-04-11 06:33:42 -0500

    Finalized CRUD operations on Categories & Food Items for Vendors.

--- accounts/forms.py
+++ accounts/forms.py
@@ -26,8 +26,6 @@ class UserProfileForm(forms.ModelForm):
     profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
     cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
 
-    # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-    # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
     class Meta:
         model = UserProfile
         fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
